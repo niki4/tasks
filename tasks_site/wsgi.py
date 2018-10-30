2ec8c212d818e4ada0cc20d48a34e5082c14248d
Ivan Nikiforov <nikiforr@gmail.com>
2018-10-31 00:23:21 +0300

    Use django-configurations to allow change settings with OS Env

--- tasks_site/wsgi.py
+++ tasks_site/wsgi.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tasks_site.settings')
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
+
+from configurations.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
