3cb79a448e3daab20975a9a3e2ffb4e5cc6786bf
Ivan Nikiforov <nikiforr@gmail.com>
2018-10-30 11:37:07 +0300

    Add jsonschema usage examples

--- example_tasks.py
+++ example_tasks.py
@@ -3,13 +3,26 @@ from functools import reduce
 import tasks
 
 
-@tasks.task(name='multiprint')
+@tasks.task(name='multiprint',
+            json_schema={'type': 'object',
+                         'properties': {
+                             'msg': {'type': 'string'},
+                             'count': {'type': 'integer', 'minimum': 1}
+                         },
+                         'required': ['msg']})
 def multi_print(msg, count=10):
     return '\n'.join(msg for _ in range(count))
 
 
 class Multiply(tasks.BaseTask):
     name = 'mult'
+    json_schema = {'type': 'object',
+                   'properties': {
+                       'operands': {'type': 'array',
+                                    'minItems': 1,
+                                    'items': {'type': 'number'}}
+                   },
+                   'required': ['operands']}
 
     def run(self, operands):
         return reduce(lambda x, y: x*y, operands)
